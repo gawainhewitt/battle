d9795419c5547ace05bd2b83890227d3478b0eb9
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-05-06 22:54:29 +0100

    added test and solution for names

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -5,9 +5,16 @@ require_relative "../../app"
 
 Capybara.app = Battle
 
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit("/")
-    expect(page).to have_content 'Testing infrastructure working!'
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Gawain'
+    fill_in :player_2_name, with: 'Alex'
+    click_button 'Submit'
+
+    # save_and_open_page
+
+    expect(page).to have_content "Gawain vs. Alex"
   end
 end
+
